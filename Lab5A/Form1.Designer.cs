2acd481d01c8fbb5e45a1da241985300e9f97951
CHIRAG M BARANDA <45106856+chiragmb96@users.noreply.github.com>
2020-01-13 15:56:01 -0500

    Add files via upload

---
+++ Lab5A/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Lab5A
+{
+    partial class Lab5A_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.cdlColorChooser = new System.Windows.Forms.ColorDialog();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(46, 12);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(75, 23);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(166, 12);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // cdlColorChooser
+            // 
+            this.cdlColorChooser.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Location = new System.Drawing.Point(46, 82);
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(104, 45);
+            this.speedTrackBar.TabIndex = 0;
+            this.speedTrackBar.TickFrequency = 10;
+            this.speedTrackBar.Scroll += new System.EventHandler(this.speedTrackBar_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab5A.Properties.Resources.Faucet;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 69);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Lab5A_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(309, 425);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.ColorButton);
+            this.Name = "Lab5A_form";
+            this.Text = "A Drop In The Bucket";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.paintForm);
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ColorDialog cdlColorChooser;
+        private System.Windows.Forms.TrackBar speedTrackBar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab5A/Form1.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * I, CHIRAG BARANDA, student number 000759867, 
+ * certify that all code submitted is my own work; 
+ * that I have not copied it from any other source.  
+ * I also certify that I have not allowed my work to be copied by others.
+ * 
+ * Author: Chirag Baranda
+ * Student Number: 000759867
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5A
+{
+    public partial class Lab5A_form : Form
+    {
+        //defalut valeus for variales
+        private Color c = Color.Blue;
+        Graphics draw;
+        Pen pen = new Pen(Color.White);
+        private SolidBrush b;
+
+        //properties for bucket filling water, x and y -axies , heiht and width 
+        int x = 101, y = 396, w = 200, h = 4;
+
+        //properties for water falinf from tap, x and y -axies , heiht and width 
+        int x1 = 115, y1 = 200, w1 = 15 , h1 = 200;
+
+        /// <summary>
+        /// initalize form for some defult values
+        /// </summary>
+        public Lab5A_form()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(paintForm);
+        }
+
+
+        /// <summary>
+        /// color choosing on button click
+        /// open color dilouge box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            cdlColorChooser.Color = c;                //Display with the previous colour already chosen
+            cdlColorChooser.ShowDialog();             //Display the actual dialog box
+            c = cdlColorChooser.Color;
+        }
+
+
+        /// <summary>
+        /// exiting environment
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// drawing bucket
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void paintForm(object sender, PaintEventArgs e)
+        {
+            draw = e.Graphics;
+            draw = this.CreateGraphics();
+            b = new SolidBrush(c); //Color.YellowGreen
+            draw.DrawLine(pen, 100, 290, 100, 400);
+            draw.DrawLine(pen, 100, 400, 300, 400);
+            draw.DrawLine(pen, 300, 400, 300, 290);
+            //draw.Dispose();
+        }
+
+        /// <summary>
+        /// control water falling from tap as per trackbar speed and eable and disavle the water falling/timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void speedTrackBar_Scroll(object sender, EventArgs e)
+        {
+            draw = this.CreateGraphics();
+            timer1.Enabled = true;
+            timer1.Interval = 120 - speedTrackBar.Value * 10;
+            if(h>80 && timer1.Enabled == false)
+            {
+                draw.FillRectangle(new SolidBrush(Color.Black), x, y, w, h);
+            }
+            
+        }
+
+
+        /// <summary>
+        /// timer takes value from speed track bar and control water falling speed
+        /// if the bucket is filled up, it stop pouring water in bucket from tap\and reset the bucket water to empty
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                //filled up bucket water
+                draw.FillRectangle(b, x, y, w, h);
+                
+                //bucket filling watter
+                draw.FillRectangle(b, x, y, w, h);
+
+                y -= 1;
+                h += 1;
+
+                //drawing water falling from tap
+                draw.FillRectangle(b, x1, y1, w1, h1);
+
+                h1 -= 10;
+
+                //when bucket is filled up, stop puring water from tap
+                if (h > 80)
+                {
+                    draw.FillRectangle(new SolidBrush(Color.Black), x1, y1, w1, 120); //setting background to black so it seems container is empty
+                    timer1.Enabled = false; //stop timer
+                    speedTrackBar.Value = speedTrackBar.Minimum; //reset the speed to minimum
+
+                }
+            }
+        }
+                   
+            
+    }
+}
